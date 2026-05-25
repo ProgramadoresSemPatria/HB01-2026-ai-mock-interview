f500a455d36ce84cb5dd9cb9a4baf1a89584ec7d
ViniNathan <viniciosnathan@gmail.com>
2026-05-25 19:37:13 -0300

    project initialization

---
+++ packages/ui/src/components/skeleton.tsx
@@ -0,0 +1,13 @@
+import { cn } from "@hackathon2026/ui/lib/utils";
+
+function Skeleton({ className, ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="skeleton"
+      className={cn("animate-pulse rounded-none bg-muted", className)}
+      {...props}
+    />
+  );
+}
+
+export { Skeleton };
