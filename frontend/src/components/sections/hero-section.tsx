370256efa74ddd0d8bfc69399e6b4770b9eb47fb
ViniNathan <viniciosnathan@gmail.com>
2026-07-10 07:40:29 -0300

    feat: enhance HeroSection with framer-motion for animated text and improved layout

--- frontend/src/components/sections/hero-section.tsx
+++ frontend/src/components/sections/hero-section.tsx
@@ -2,6 +2,7 @@
 
 import { type RefObject } from "react";
 import ImageParallax from "../landing-page/image-parallax";
+import { motion } from "framer-motion";
 import heroImage from "../../assets/logo.png";
 
 interface HeroSectionProps {
@@ -13,24 +14,40 @@ const HeroSection = ({ parallaxRef }: HeroSectionProps) => {
     <section
       style={{
         display: "flex",
-        justifyContent: "center",
-        alignItems: "flex-end",
+        flexDirection: "column",
+        justifyContent: "flex-end",
+        alignItems: "center",
+        gap: "20px",
         height: "100vh",
       }}
     >
-      <ImageParallax
-        ref={parallaxRef}
-        src={heroImage}
-        alt="Hone"
-        width="min(75vw, 800px)"
-        height="min(75vw, 800px)"
-        intensity={100}
-        overscan={35}
-        noiseOpacity={0.35}
-        objectPosition="center"
-        noiseIntensity={30}
-        startOffset={-30}
-      />
+      <motion.div className="text-center text-9xl font-normal text-white instrument-serif"
+      style={{ y: -300, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ duration: 2, ease: "easeInOut" }}
+      >
+        AI MOCK INTERVIEW
+      </motion.div>
+      <motion.div
+      style={{opacity: 0 }}
+      animate={{opacity: 1 }}
+      transition={{ duration: 2, ease: "easeInOut" }}
+      >
+        <ImageParallax
+          ref={parallaxRef}
+          src={heroImage}
+          alt="Hone"
+          width="min(75vw, 800px)"
+          height="min(75vw, 800px)"
+          intensity={100}
+          overscan={35}
+          noiseOpacity={0.35}
+          objectPosition="center"
+          noiseIntensity={30}
+          startOffset={-30}
+        />
+      </motion.div>
+
     </section>
   );
 };
